ecb05b5254752454a335c33ccd67e93353bb2c17
Viktor Lukovets <nuerio63@gmail.com>
2019-10-23 12:32:26 +0300

    modified solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const page = document.documentElement;
+const counter = 'counter';
 const counterContainer = document.querySelector('#counter');
-if (!sessionStorage.hasOwnProperty('counter')) {
-  sessionStorage.setItem('counter', 0);
+if (!sessionStorage.getItem(counter)) {
+  sessionStorage.setItem(counter, 0);
 }
-counterContainer.textContent = sessionStorage.getItem('counter');
+counterContainer.textContent = sessionStorage.getItem(counter);
 
 function changeCount() {
   sessionStorage.counter = Number(sessionStorage.counter) + 1;
-  counterContainer.textContent = sessionStorage.getItem('counter');
+  counterContainer.textContent = sessionStorage.getItem(counter);
 }
 
 page.addEventListener('click', changeCount);
